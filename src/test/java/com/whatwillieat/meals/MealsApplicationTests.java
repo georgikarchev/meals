e086262785c34269d814968ca39e814dd7b561ca
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-16 21:36:53 +0200

    Initial commit - Create Spring Boot App

---
+++ src/test/java/com/whatwillieat/meals/MealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatwillieat.meals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
