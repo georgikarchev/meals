311ed443cbb66b8ca02cce1fd9d436351bc25555
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-23 00:11:34 +0200

    Add meal and ingredient DTOs

---
+++ src/main/java/com/whatwillieat/meals/web/dto/IngredientRequest.java
@@ -0,0 +1,9 @@
+package com.whatwillieat.meals.web.dto;
+
+import com.whatwillieat.meals.model.DietaryCategory;
+
+public class IngredientRequest {
+    private String name;
+    private String description;
+    private DietaryCategory dietaryCategory;
+}
---
+++ src/main/java/com/whatwillieat/meals/web/dto/MealRequest.java
@@ -0,0 +1,33 @@
+package com.whatwillieat.meals.web.dto;
+
+import com.whatwillieat.meals.model.DietaryCategory;
+import com.whatwillieat.meals.model.MealType;
+import jakarta.annotation.Nullable;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MealRequest {
+    @NotNull
+    private String name;
+
+    @Nullable
+    private String description;
+
+    @NotNull
+    private Set<DietaryCategory> dietaryCategories;
+
+    @NotNull
+    private Set<MealType> mealType;
+
+    @NotNull
+    private LocalDateTime consumedOn;
+}
