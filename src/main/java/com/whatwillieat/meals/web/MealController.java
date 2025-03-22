d282b344e1196eb155b112fd1a8a2f14a2fae49a
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-23 00:23:05 +0200

    Remove MealSuggestion logic from service
    This logic will better be handled in a front-end context to increase speed and responsiveness to user interactions.

--- src/main/java/com/whatwillieat/meals/web/MealController.java
+++ src/main/java/com/whatwillieat/meals/web/MealController.java
@@ -1,37 +1,75 @@
 package com.whatwillieat.meals.web;
 
-import com.whatwillieat.meals.model.DietaryCategory;
-import com.whatwillieat.meals.model.MealType;
+import com.whatwillieat.meals.model.*;
+import com.whatwillieat.meals.service.IngredientService;
 import com.whatwillieat.meals.service.MealService;
-import com.whatwillieat.meals.service.MealSuggestionService;
-import com.whatwillieat.meals.dto.MealSuggestion;
+import com.whatwillieat.meals.web.dto.MealRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("${app.API_V1_BASE_URL}/meals")
 public class MealController {
     private final MealService mealService;
-    private final MealSuggestionService suggestionService;
+    private final IngredientService ingredientService;
 
-    public MealController(MealService mealService, MealSuggestionService suggestionService) {
+    public MealController(MealService mealService, IngredientService ingredientService) {
         this.mealService = mealService;
-        this.suggestionService = suggestionService;
+        this.ingredientService = ingredientService;
     }
 
-    @GetMapping("/{userId}/suggestions")
-    public ResponseEntity<List<MealSuggestion>> getSuggestions(
-            @PathVariable("userId") UUID userId,
-            @RequestParam(required = false) MealType mealType,
-            @RequestParam(required = false) DietaryCategory dietaryCategory) {
+    @PostMapping
+    public ResponseEntity<Meal> createMeal(@RequestBody MealRequest mealRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                mealService.saveMeal(Meal.builder()
+                        .name(mealRequest.getName())
+                        .dietaryCategories(mealRequest.getDietaryCategories())
+                        .mealTypes(mealRequest.getMealType())
+                        .build())
+        );
+    }
 
-        List<MealSuggestion> suggestions =
-                suggestionService.getSuggestions(userId, mealType, dietaryCategory);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMeal(@PathVariable UUID id) {
+        mealService.softDeleteMeal(id);
+        return ResponseEntity.noContent().build();
+    }
 
-        return  ResponseEntity.ok()
-                .body(suggestions);
+    @GetMapping("/{id}")
+    public ResponseEntity<Meal> getMeal(@PathVariable UUID id) {
+        return ResponseEntity.ok(mealService.getMeal(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Meal>> getMeals() {
+        return ResponseEntity.ok(mealService.getAllMeals());
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> getMealsCount() {
+        return ResponseEntity.ok(mealService.getMealsCount());
+    }
+
+    @PostMapping("/{mealId}/ingredients")
+    public ResponseEntity<Void> addIngredientToMeal(
+            @PathVariable UUID mealId,
+            @RequestBody UUID ingredientId,
+            @RequestBody double quantity,
+            @RequestBody UnitOfMeasurement unitOfMeasurement) {
+        Meal meal = mealService.getMeal(mealId);
+        Ingredient ingredient = ingredientService.getIngredient(ingredientId);
+        mealService.addIngredientToMeal(meal, ingredient, quantity, unitOfMeasurement);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{mealId}/ingredients/{ingredientId}")
+    public ResponseEntity<Void> removeIngredientFromMeal(@PathVariable UUID mealId, @PathVariable UUID ingredientId) {
+        mealService.removeIngredientFromMeal(mealId, ingredientId);
+        return ResponseEntity.noContent().build();
     }
 }
