9c5f4e586fc5826f37b000de6b94cd00c3f14033
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-23 01:02:52 +0200

    Add Ingredient controller

---
+++ src/main/java/com/whatwillieat/meals/web/IngredientController.java
@@ -0,0 +1,51 @@
+package com.whatwillieat.meals.web;
+
+import com.whatwillieat.meals.model.DietaryCategory;
+import com.whatwillieat.meals.model.Ingredient;
+import com.whatwillieat.meals.service.IngredientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("${app.API_V1_BASE_URL}/ingredients")
+public class IngredientController {
+    private final IngredientService ingredientService;
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
+        return ResponseEntity.ok(ingredientService.save(ingredient));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Ingredient> softDeleteIngredient(@PathVariable UUID id) {
+        ingredientService.softDelete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ingredient> getIngredient(@PathVariable UUID id) {
+        return ResponseEntity.ok(ingredientService.getIngredient(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ingredient>> getAllIngredients() {
+        return ResponseEntity.ok(ingredientService.getIngredients());
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> getIngredientCount() {
+        return ResponseEntity.ok(ingredientService.getIngredientCount());
+    }
+
+    @GetMapping("/by-category")
+    public ResponseEntity<List<Ingredient>> getIngredientsByCategory(@RequestParam DietaryCategory category) {
+        return ResponseEntity.ok(ingredientService.getIngredientsByDietaryCategory(category));
+    }
+}
